5a006845833e4a17f0037a984d0f368c7797f1ec
VilladsHJ <Villads.joergensen@gmail.com>
2024-10-14 10:49:00 +0200

    added sorting to report

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     #print(f"{words} words in the text")
     #print(character_counter(text))
     book_report(book_path, text, words)
-
+    #char_sorter(text)
 # Function that takes a path to a book text and returns the text
 def book_text(path):
     with open(path) as f:
@@ -27,12 +27,26 @@ def character_counter(text):
             character_counter[char] = 1
     return character_counter
 
+def char_sorter(text):
+    sorted_list = []
+    sorted_dictionary ={}
+    for k in character_counter(text):
+        sorted_list.append((character_counter(text)[k],k))
+    sorted_list = sorted(sorted_list, reverse=True)
+    for i in range(0,len(sorted_list)):
+        sorted_dictionary[sorted_list[i][1]] = sorted_list[i][0]
+    return sorted_dictionary
+
 def book_report(book_path, text, count):
+    final_dict = char_sorter(text)
     print(f"___ Report for {book_path} ___")
     print(f"The text contains {count} words")
 
-    for k in character_counter(text):
-        print(f"{k} is found {character_counter(text)[k]} times")
+    for k in final_dict:
+        if final_dict[k] == 1:
+            print(f"{k} is found {final_dict[k]} time")
+        else:
+            print(f"{k} is found {final_dict[k]} times")
 
 main()
 
